945a20ba0f85f5add74e3c83e68171280a0e29db
utarid <utarid@protonmail.com>
2021-02-07 19:48:29 +0300

    servis yazıldı

--- app/src/main/java/org/utarid/foregroundservice/ForegroundService.java
+++ app/src/main/java/org/utarid/foregroundservice/ForegroundService.java
@@ -1,12 +1,46 @@
 package org.utarid.foregroundservice;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 
 public class ForegroundService extends Service {
+
+    @Override
+    public void onCreate() {
+        startForeground(1, createNotification());
+    }
+
+    public Notification createNotification() {
+        String CHANNEL_ID = "CHANNEL";
+        NotificationChannel serviceChannel = new NotificationChannel(
+                CHANNEL_ID,
+                "channel name",
+                NotificationManager.IMPORTANCE_NONE
+        );
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(serviceChannel);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, CHANNEL_ID);
+        Notification notification = notificationBuilder
+                .setContentTitle("title")
+                .setContentText("text")
+                .setSmallIcon(R.drawable.ic_services)
+                .build();
+
+        return notification;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
--- app/src/main/java/org/utarid/foregroundservice/MainActivity.java
+++ app/src/main/java/org/utarid/foregroundservice/MainActivity.java
@@ -1,5 +1,8 @@
 package org.utarid.foregroundservice;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -9,27 +12,49 @@ import org.utarid.foregroundservice.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        org.utarid.foregroundservice.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        Intent intent = new Intent(this, ForegroundService.class);
 
         binding.btnStartService.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                startMyService(intent);
             }
         });
 
         binding.btnStopService.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                stopMyService(intent);
             }
         });
+    }
+
+    public void startMyService(Intent intent) {
+        if (!checkIfServiceIsRunning()) {
+            startForegroundService(intent);
+        }
+    }
+
+    public void stopMyService(Intent intent) {
+        if (checkIfServiceIsRunning()) {
+            stopService(intent);
+        }
+    }
+
+    public boolean checkIfServiceIsRunning() {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (ForegroundService.class.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
