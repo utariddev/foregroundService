ae901c8fbe85f16eb4745cc7acab993705cc4a56
utarid <utarid@protonmail.com>
2021-02-07 16:43:03 +0300

    servis sinifi olusturuldu

---
+++ app/src/main/java/org/utarid/foregroundservice/ForegroundService.java
@@ -0,0 +1,15 @@
+package org.utarid.foregroundservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class ForegroundService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
