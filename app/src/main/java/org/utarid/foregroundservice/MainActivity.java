7eb4ce8daf9d39bd34ca985f0e10794b9ee8b6be
utarid <utarid@protonmail.com>
2021-02-07 17:17:17 +0300

    arayuz yazildi

--- app/src/main/java/org/utarid/foregroundservice/MainActivity.java
+++ app/src/main/java/org/utarid/foregroundservice/MainActivity.java
@@ -1,14 +1,35 @@
 package org.utarid.foregroundservice;
 
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import org.utarid.foregroundservice.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.btnStartService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        binding.btnStopService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
